de81b1674aeae1b124e52dd7dcd5a7786ead2a14
woodenstick <309136832@qq.com>
2019-04-10 19:14:07 +0800

    39. 数组总和

---
+++ src/leetcode/CombinationSum.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 39. 数组总和
+ * 给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+ * 
+ * candidates 中的数字可以无限制重复被选取。
+ * 
+ * 说明：
+ * 
+ * 所有数字（包括 target）都是正整数。 解集不能包含重复的组合。
+ * 
+ * @author stick
+ *
+ */
+public class CombinationSum {
+
+	public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> listAll=new ArrayList<List<Integer>>();
+        List<Integer> list=new ArrayList<Integer>();
+        //排序
+        Arrays.sort(candidates);
+        find(listAll,list,candidates,target,0);
+        return listAll;
+    }
+    public void find(List<List<Integer>> listAll,List<Integer> tmp,int[] candidates, int target,int num){
+        //递归的终点
+        if(target==0){
+            listAll.add(tmp);
+            return;
+        } 
+        if(target<candidates[0]) return;
+        for(int i=num;i<candidates.length&&candidates[i]<=target;i++){
+            //深拷贝
+            List<Integer> list=new ArrayList<>(tmp);
+            list.add(candidates[i]);
+            //递归运算，将i传递至下一次运算是为了避免结果重复。
+            find(listAll,list,candidates,target-candidates[i],i);
+        }   
+    }
+    
+    public static void main(String[] args) {
+    	CombinationSum test = new CombinationSum();
+    	System.out.println(test.combinationSum(new int[]{2,3,6,7}, 7).toString());
+	}
+}
