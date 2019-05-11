1431439ad90a61d8ec75872a4026e41dec82e0dc
woodenstick <309136832@qq.com>
2019-05-11 19:58:47 +0800

    75.颜色分类

---
+++ src/leetcode2/SortColors.java
@@ -0,0 +1,25 @@
+package leetcode2;
+
+public class SortColors {
+
+	public void sortColors(int[] nums) {
+        // 三指针
+        int i=0, l=0, r=nums.length-1;
+        while(i<=r){
+            if(i<l || nums[i]==1)
+                i++;
+            else if(nums[i]==0)
+                swap(nums,i,l++);
+            else if(nums[i]==2)
+                swap(nums,i,r--);
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j){
+        if(i==j)
+            return;
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
