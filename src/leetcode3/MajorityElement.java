a8b487221377d3f79a44b018b0fcd26c98d545ef
woodenstick <309136832@qq.com>
2019-06-25 21:57:59 +0800

    众数

--- src/leetcode3/MajorityElement.java
+++ src/leetcode3/MajorityElement.java
@@ -1,6 +1,7 @@
 package leetcode3;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * 众数
@@ -33,4 +34,33 @@ public class MajorityElement {
         Arrays.sort(nums);
         return nums[nums.length/2];
     }
+	
+	private int randRange(Random rand, int min, int max) {
+        return rand.nextInt(max - min) + min;
+    }
+
+    private int countOccurences(int[] nums, int num) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == num) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int majorityElement3(int[] nums) {
+        Random rand = new Random();
+
+        int majorityCount = nums.length/2;
+
+        while (true) {
+            int candidate = nums[randRange(rand, 0, nums.length)];
+            if (countOccurences(nums, candidate) > majorityCount) {
+                return candidate;
+            }
+        }
+    }
+	
+	
 }
