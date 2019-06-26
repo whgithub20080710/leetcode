1d36e50e74237865b8033b6e03af444b8f3ca4b9
woodenstick <309136832@qq.com>
2019-06-26 21:45:14 +0800

    众数，这个解法绝了

--- src/leetcode3/MajorityElement.java
+++ src/leetcode3/MajorityElement.java
@@ -61,6 +61,22 @@ public class MajorityElement {
             }
         }
     }
+    
+    public int majorityElement4(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+    
+    
 	
 	
 }
