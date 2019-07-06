4905d47bfc1968e66781369ec15cec83ad4cbc2d
woodenstick <309136832@qq.com>
2019-07-06 18:59:45 +0800

    191. 位1的个数

--- src/leetcode3/HammingWeight.java
+++ src/leetcode3/HammingWeight.java
@@ -1,5 +1,10 @@
 package leetcode3;
 
+/**
+ * 191. 位1的个数
+ * @author stick
+ *
+ */
 public class HammingWeight {
 
 	public int hammingWeight(int n) {
@@ -9,6 +14,11 @@ public class HammingWeight {
 	        n &= (n - 1);
 	    }
 	    return sum;
+	}
+	
+	public static void main(String[] args) {
+		HammingWeight test = new HammingWeight();
+		System.out.println(test.hammingWeight(8));
 	}
 
 }
