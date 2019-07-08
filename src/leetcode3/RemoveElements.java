da7b0a1e03492e44616e563dd90a64baa51d61a2
woodenstick <309136832@qq.com>
2019-07-08 21:42:05 +0800

    203. 移除链表元素

---
+++ src/leetcode3/RemoveElements.java
@@ -0,0 +1,22 @@
+package leetcode3;
+
+import leetcode.ListNode;
+
+public class RemoveElements {
+
+	public ListNode removeElements(ListNode head, int val) {
+        //创建一个虚拟头结点
+        ListNode dummyNode=new ListNode(val-1);
+        dummyNode.next=head;
+        ListNode prev=dummyNode;
+        //确保当前结点后还有结点
+        while(prev.next!=null){
+            if(prev.next.val==val){
+                prev.next=prev.next.next;
+            }else{
+                prev=prev.next;
+            }
+        }
+        return dummyNode.next;
+    }
+}
