e45bb55fa794936bd3f40b3b51c1cc15f0cf01c6
woodenstick <309136832@qq.com>
2019-07-14 20:12:44 +0800

    205. 同构字符串

---
+++ src/leetcode3/IsIsomorphic.java
@@ -0,0 +1,28 @@
+package leetcode3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IsIsomorphic {
+
+	public boolean isIsomorphic(String s, String t) {
+		Map<Character, Character> map = new HashMap<>();
+		if (s.length() != t.length())
+			return false;
+
+		for (int i = 0; i < s.length(); i++) {
+			char ss = s.charAt(i);
+			char tt = t.charAt(i);
+			if (map.containsKey(ss)) {
+				if (map.get(ss) != tt)
+					return false;
+			} else {
+				if (map.containsValue(tt))
+					return false;
+				map.put(ss, tt);
+			}
+
+		}
+		return true;
+	}
+}
