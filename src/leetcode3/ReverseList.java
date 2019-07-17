2984adc9fcb6c7a4f5369b947704e08c9d59b65d
woodenstick <309136832@qq.com>
2019-07-17 20:34:57 +0800

    206. 反转链表

---
+++ src/leetcode3/ReverseList.java
@@ -0,0 +1,18 @@
+package leetcode3;
+
+import leetcode.ListNode;
+
+public class ReverseList {
+
+	public ListNode reverseList1(ListNode head) {
+	    ListNode prev = null;
+	    ListNode curr = head;
+	    while (curr != null) {
+	        ListNode nextTemp = curr.next;
+	        curr.next = prev;
+	        prev = curr;
+	        curr = nextTemp;
+	    }
+	    return prev;
+	}
+}
