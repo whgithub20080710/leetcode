459d43a2719b310f1226f1123b36ff3fe36e0198
woodenstick <309136832@qq.com>
2019-07-29 22:29:02 +0800

    ContainsDuplicate

---
+++ src/leetcode3/ContainsDuplicate.java
@@ -0,0 +1,15 @@
+package leetcode3;
+
+import java.util.Arrays;
+
+public class ContainsDuplicate {
+
+	public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; ++i) {
+            if (nums[i] == nums[i + 1]) return true;
+        }
+        return false;
+
+    }
+}
