5084b8356609dcf2e1576a337786317db1a76cd0
woodenstick <309136832@qq.com>
2019-07-31 21:47:30 +0800

    217. 存在重复元素

--- src/leetcode3/ContainsDuplicate.java
+++ src/leetcode3/ContainsDuplicate.java
@@ -2,6 +2,13 @@ package leetcode3;
 
 import java.util.Arrays;
 
+/**
+ * 217. 存在重复元素
+ * 给定一个整数数组，判断是否存在重复元素。
+ * 如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
+ * @author stick
+ *
+ */
 public class ContainsDuplicate {
 
 	public boolean containsDuplicate(int[] nums) {
@@ -10,6 +17,5 @@ public class ContainsDuplicate {
             if (nums[i] == nums[i + 1]) return true;
         }
         return false;
-
     }
 }
